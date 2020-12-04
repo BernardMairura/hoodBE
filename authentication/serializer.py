02360aa906d5beed1ed6d74db095005f09740daa
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-12-04 20:23:40 +0300

    password validation

--- authentication/serializer.py
+++ authentication/serializer.py
@@ -20,4 +20,52 @@ class RegistrationSerializer(serializers.ModelSerializer):
         }
     )
  
-   
+    # characters, and can not be read by the client.
+    password = serializers.CharField(
+            required=True,
+            allow_null=False,
+            write_only=True,
+            min_length=6,
+    )
+    confirm_password = serializers.CharField(
+            write_only=True,
+            required=True,
+            allow_null=False,
+            min_length=6,)
+    user_name = serializers.CharField(
+            required=True,
+            allow_null=False,
+            min_length=2,
+            validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="Username already exist",
+            )
+        ],
+        error_messages={
+            'required': "Username is a required field",
+        }
+    )
+    # controls how a client handles a token when doing registration
+    
+    token = serializers.CharField(read_only=True)
+
+    @staticmethod
+    def validate_password_(data):
+        password = data.get('password', None)
+        confirm_password = data.get('password', None)
+        # Raises an exception where username and password is not provided.
+       
+        if confirm_password != password:
+            raise serializers.ValidationError(
+                'Confirmed Password and Password should match .'
+            )
+        return data 
+
+    class Meta:
+        model = User
+        # List all of the fields that could possibly be included in a request
+        # or response, including fields specified explicitly above.
+        fields = ['user_name', 'email',
+                  'password','confirm_password','is_supervisor','is_manager','is_occupant',  'token']
+    
